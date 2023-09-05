3e0376c11ea1e8fc225ad8fcca7efbd869b72f8f
sadidnoovosoft <sadid.shaikh@noovosoft.com>
2023-09-05 22:41:48 +0530

    Create express server

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+
+//...
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
