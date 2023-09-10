d5f60c839a133ecb98a2a4aa7b73ad2388df3696
sadidnoovosoft <sadid.shaikh@noovosoft.com>
2023-09-10 15:37:49 +0530

    Incorporate suggested changes during code review

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-dotenv.config();
+
 import auth from "./routes/auth.js";
 import userRouter from "./routes/users.js";
 import transactionRouter from "./routes/transactions.js";
 
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.APP_PORT;
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use("/api/auth", auth);
-app.use("/api/users", userRouter);
-app.use("/api/transactions", transactionRouter);
+// API routes
+const apiRouter = express.Router();
+app.use("/api", apiRouter);
+
+apiRouter.use("/auth", auth);
+apiRouter.use("/users", userRouter);
+apiRouter.use("/transactions", transactionRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
--- connection.js
+++ connection.js
@@ -1,13 +1,27 @@
 import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
 
-const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-})
+// Singleton class that will only return one instance of pool
+class Database {
+    constructor() {
+        this.pool = new pg.Pool({
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_DATABASE,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT,
+        })
+    }
 
-export default pool;
+    static getPool() {
+        if (!this.instance) {
+            this.instance = new Database();
+        }
+        return this.instance.getPool();
+    }
+
+    getPool() {
+        return this.pool;
+    }
+}
+
+export default Database.getPool();
